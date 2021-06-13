212db54fb76e634c4ca0fc5deeeba83352179e6f
Jeremias-V <jeremias.villalobos.t@gmail.com>
2021-06-13 12:37:44 -0500

    uDebug xdg-open script

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ In this repository I explore shell scripting on Linux by creating scripts that c
 ## [UVa-Local-Judge](https://github.com/Jeremias-V/Linux-Shell-Utilities/tree/master/UVa-Local-Judge)
 This is a simple and small proyect where I try to simulate what https://onlinejudge.org/ does when one submits a source code file.
 Might add more functionality later, for now it only works with C++11.
+
+## [uDebug-Utils](https://github.com/Jeremias-V/Linux-Shell-Utilities/tree/master/uDebug-Utils)
+Here you can find a script that opens the page to the given UVa ID at https://www.udebug.com/UVa with your default web browser. 
---
+++ uDebug-Utils/open-udebug.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+# Simple tool to open uDebug page for the given UVa problem ID number.
+# I will clean the code using functions later.
+
+# Terminal Colors
+RED='\e[1;31m'
+GREEN='\e[1;32m'
+WHITE='\e[0m'
+CYAN='\e[1;36m'
+YELLOW='\e[1;33m'
+
+# File count variables
+pyfiles=$(ls *.py 2>/dev/null | wc -l)
+cppfiles=$(ls *.cpp 2>/dev/null | wc -l)
+cfiles=$(ls *.c 2>/dev/null | wc -l)
+
+if [ $pyfiles -eq 1 -a $cppfiles -eq 0 -a $cfiles -eq 0 ]
+
+    # Only found 1 python file
+    then 
+    ID=$(ls *.py | awk '{i=index($1,"."); print substr($1,0,i-1)}')
+    echo -e "\n${GREEN}Opening${WHITE} uDebug UVa ${CYAN}${ID}${WHITE} at https://www.udebug.com/UVa/$ID\n"
+    xdg-open https://www.udebug.com/UVa/$ID
+    exit 0
+
+elif [ $pyfiles -eq 0 -a $cppfiles -eq 1 -a $cfiles -eq 0 ]
+
+    # Only found 1 C++ file
+    then
+    ID=$(ls *.cpp | awk '{i=index($1,"."); print substr($1,0,i-1)}')
+    echo -e "\n${GREEN}Opening${WHITE} uDebug UVa ${CYAN}${ID}${WHITE} at https://www.udebug.com/UVa/$ID\n"
+    xdg-open https://www.udebug.com/UVa/$ID
+    exit 0
+
+elif [ $pyfiles -eq 0 -a $cppfiles -eq 0 -a $cfiles -eq 1 ]
+
+    # Only found 1 Ansi C file
+    then
+    ID=$(ls *.c | awk '{i=index($1,"."); print substr($1,0,i-1)}')
+    echo -e "\n${GREEN}Opening${WHITE} uDebug UVa ${CYAN}${ID}${WHITE} at https://www.udebug.com/UVa/$ID\n"
+    xdg-open https://www.udebug.com/UVa/$ID
+    exit 0
+
+elif [ $pyfiles -eq 0 -a $cppfiles -eq 0 -a $cfiles -eq 0 ]
+
+    # Didn't found any accepted file.
+    then
+    echo -e "\n${RED}Error:${WHITE} Couldn't find any file with a valid extension ${CYAN}*.{py, cpp, c}${WHITE}.\n"
+    exit 1
+
+else
+
+    # Found multiple valid files.
+    echo -e "\n${YELLOW}Warning:${WHITE} Multiple files with valid extensions (${CYAN}*.{py, cpp, c}${WHITE}) found."
+    read -p "Enter the numeric problem ID -> " ID
+
+    count=$(ls $ID.* 2> /dev/null | wc -l)
+
+    if [ $count -ne 0 ]
+
+        # Given ID is valid (i.e. in current working directory).
+        then
+        echo -e "\n${GREEN}Opening${WHITE} uDebug UVa ${CYAN}${ID}${WHITE} at https://www.udebug.com/UVa/$ID\n"
+        xdg-open https://www.udebug.com/UVa/$ID
+        exit 0
+
+    else
+
+        # Given ID is not in current directory.
+        validIDs=$(find -iname '*.py' -or -iname '*.cpp' -or -iname '*.c' 2>/dev/null | awk '{i=index($1,"."); print substr($1,3)}')
+        countValid=$(echo $validIDs | wc -l)
+
+        if [ $countValid -eq 0 ]
+
+            # No valid file exists
+            then
+            echo -e "\n${RED}Error:${WHITE} Couldn't find any valid file in current directory, move to your working directory.\n"
+            exit 1
+
+        else
+
+            # Couldn't find the given ID but found multiple valid files.
+            echo -e "\n${RED}Error:${WHITE} The given ID is not in the current directory, try the UVa ID of any of these files.\n${CYAN}${validIDs}${WHITE}\n"
+            exit 0
+
+        fi
+
+    fi
+
+fi
