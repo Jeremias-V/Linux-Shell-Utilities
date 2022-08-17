737c7e03987e537d59560511b41a65f2d366a11a
Jeremias-V <jeremias.villalobos.t@gmail.com>
2022-08-17 12:07:48 -0500

    Cleaner btconnect script and file structure

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 In this repository I explore shell scripting on Linux by creating scripts that can be quite useful for various things via Bash.
 
 
-## [UVa-Local-Judge](https://github.com/Jeremias-V/Linux-Shell-Utilities/tree/master/UVa-Local-Judge)
+## [UVa Local Judge](https://github.com/Jeremias-V/Linux-Shell-Utilities/tree/master/local-judge.sh)
 This is a simple and small proyect where I try to simulate what https://onlinejudge.org/ does when one submits a source code file.
 Might add more functionality later, for now it only works with C++11.
 
-## [uDebug-Utils](https://github.com/Jeremias-V/Linux-Shell-Utilities/tree/master/uDebug-Utils)
+## [uDebug Utils](https://github.com/Jeremias-V/Linux-Shell-Utilities/tree/master/open-udebug.sh)
 Here you can find a script that opens the page to the given UVa ID at https://www.udebug.com/UVa with your default web browser. 
 
-## [Bluetooth-Connect](https://github.com/Jeremias-V/Linux-Shell-Utilities/tree/master/Bluetooth-Connect)
+## [Bluetooth Connect](https://github.com/Jeremias-V/Linux-Shell-Utilities/tree/master/btconnect.sh)
 A simple script to connect to previously paired devices using `bluetoothctl`. This script shows intuitive error messages and loading dots when waiting a process.
--- btconnect.sh
+++ btconnect.sh
@@ -8,12 +8,12 @@ GREEN='\e[1;32m'
 WHITE='\e[0m'
 CYAN='\e[1;36m'
 YELLOW='\e[1;33m'
-BOLD='\033[1m'
-PLAIN='\033[0m'
+ERROR="${RED}Error:${WHITE}"
 
 # Check if the parameter is correct. 
 if [ "$#" -ne 1 ]; then
-	echo -e "${RED}Error:${WHITE} Invalid number of parameters, must provide ONE parameter as the name of the bluetooth device to connect.\n"
+	echo -e "\n${ERROR} Invalid number of parameters, must provide"\
+			"ONE parameter as the name of the bluetooth device to connect.\n"
 	exit 1
 fi
 
@@ -24,7 +24,8 @@ count=$(echo -n $paired | wc -c)
 
 # Check if bluetoothctl is working and there are previously paired devices.
 if [[ "$btctl_error" == "$paired" || $count -eq 0 ]]; then
-	echo -e "${RED}Error:${WHITE} Couldn't find your paired devices, check your bluetooth configuration or pair your device.\n"
+	echo -e "\n${ERROR} Couldn't find your paired devices, check"\
+			"your bluetooth configuration or pair your device.\n"
 	exit 1
 fi
 
@@ -37,7 +38,8 @@ list_devices () { echo -e "${CYAN}${paired}${WHITE}\n"; }
 
 # Check if the given parameter matches any device.
 if [ $count -eq 0 ]; then
-	echo -e "${RED}Error:${WHITE} Couldn't find any device matching your parameter.\n\nTry with the following devices:\n"
+	echo -e "\n${ERROR} Couldn't find any device matching your"\
+			"parameter.\n\nTry with the following devices:\n"
 	list_devices
 	exit 1
 fi
@@ -45,22 +47,25 @@ fi
 # Check if the given parameter matches multiple devices (>1).
 count=$(echo $address | wc -l)
 if [ $count -gt 1 ]; then
-	echo -e "${RED}Error: Found multiple paired devices matching your pattern, specify better your device name.\n\n Try with the following devices:\n"
+	echo -e "\n${ERROR} Found multiple paired devices matching your pattern,"\
+			"specify better your device name.\n\n Try with the following devices:\n"
 	list_devices
 	exit 1
 fi
 
 # Print loading dots for indicating a process is running.
-echo -ne "${YELLOW}Trying to connect to your paired device."
+echo -ne "\n${YELLOW}Trying to connect to your paired device."
 while sleep 1; do printf "."; done &
 timeout 10 bluetoothctl connect $address &> /dev/null
 connect_code=$?
 kill $!
 echo -e "${WHITE}\n"
 
-# Check if the exit code of bluetoothctl was 0 (sucess code).
+# Check if the exit code of bluetoothctl was 0 (success code).
 if [ $connect_code -ne 0 ]; then
-	echo -e "${RED}Error:${WHITE} Something went wrong when connecting to your paired device. Check if the device is ready to connect (bluetooth is turned on and allowing connections).\n"
+	echo -e "${ERROR} Something went wrong when connecting to your paired"\
+			"device. Check if the device is ready to connect (bluetooth is"\
+			"turned on and allowing connections).\n"
 	exit 1
 else
 	echo -e "${GREEN}Success!${WHITE} connected to ${CYAN}${paired}${WHITE}.\n"
